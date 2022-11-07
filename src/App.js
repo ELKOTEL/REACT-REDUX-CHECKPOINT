769787f4ce44833e656197eda72edfcc456d5c18
ELKOTEL RABEB <elotelrabeb111@gmail.com>
2022-11-07 08:14:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Container, Heading } from '@chakra-ui/react';
+import { AddTodo } from './components/AddTodo';
+import { TodoList } from './components/TodoList';
+import { VisibilityFilter } from './components/VisibilityFilter';
+
+function App() {
+  return (
+    <Container   bgColor="rgb(247, 157, 55)" borderRadius={20}>
+    <Container maxW="container.sm"  >
+      <Heading my="4" color="rgb(13, 117, 158)">Todo List</Heading>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilter />
+    </Container>
+    </Container>
+  );
+}
+
+export default App;
